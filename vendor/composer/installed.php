4b30147dc16c0aa2398c018820fb99522c5e88d8
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-04 11:46:05 +0100

    ajout de l'autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4a98f3c819be165efcaf358bf52befc2d7a59b2c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4a98f3c819be165efcaf358bf52befc2d7a59b2c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
