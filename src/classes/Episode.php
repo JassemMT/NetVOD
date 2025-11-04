8c791671218347ca017094a836622ee9ab996d4f
Jassem <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-04 11:55:32 +0100

    Create Episode class with properties and methods

---
+++ src/classes/Episode.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace netvod\classes;
+
+class Episode
+{
+    protected ?int $id_episode = null;
+    protected int $numero;
+    protected string $titre;
+    protected string $description;
+    protected int $duree; // en secondes
+    protected string $source; // url ou chemin
+    protected string $image;
+
+    public function __construct(int $numero, string $titre, string $description, int $duree, string $source, string $image)
+    {
+        $this->numero = $numero;
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->duree = $duree;
+        $this->source = $source;
+        $this->image = $image;
+    }
+
+    public function __get(string $attr)
+    {
+        return $this->$attr ?? null;
+    }
+
+    public function __set(string $attr, $value): void
+    {
+        $this->$attr = $value;
+    }
+}
