43cc0e50e9d59c5c64743498149c953ae63f2577
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-04 11:51:11 +0100

    ajout de l'index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+delclare(strict_types=1);
+require_once __DIR__ . '/vendor/autoload.php';
+//session_start(); // après l'autoload pour éviter les erreurs de classes non trouvées
+
+use netvod\dispatcher\Dispatcher;
+
+$dispatcher = new Dispatcher();
+$dispatcher->run();
