3777bd9c5d9c8218321a0287a2e6f8f34c75a417
Jassem <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-04 12:12:33 +0100

    Create Commentaire class with properties and methods

---
+++ src/classes/Commentaire.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace NetVOD\Classes;
+
+class Commentaire
+{
+    protected int $note; // 1..5
+    protected string $contenu;
+    protected \DateTime $date;
+
+    public function __construct(int $note, string $contenu, string $date = null)
+    {
+        $this->note = $note;
+        $this->contenu = $contenu;
+        $this->date = $date ?? new \DateTime();
+    }
+
+    public function __get(string $attr)
+    {
+        return $this->$attr ?? null;
+    }
+
+    public function __set(string $attr, $value): void
+    {
+        $this->$attr = $value;
+    }
+}
