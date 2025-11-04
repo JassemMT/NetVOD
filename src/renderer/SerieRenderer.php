021b3626413df573c8bd93bda74e1e5efa72a0f4
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-04 14:19:30 +0100

    renderer serie

--- src/renderer/SerieRenderer.php
+++ src/renderer/SerieRenderer.php
@@ -2,13 +2,25 @@
 declare(strict_types= 1);
 namespace netvod\renderer;
 
-use netvod\classes\Serie;
+use netvod\classes\Episode;
+use netvod\renderer\EpisodeRenderer;
 
 class SerieRenderer implements Renderer {
-    public function render(array $params): string {
-        serie = (Serie)$params["serie"];
+    public function render(array $params = []): string {
+        $serie = $params["serie"];
+        $episodes = "";
         foreach ($serie->episodes as $episode) {
-            // Render each episode (implementation not shown)
+            $episodes .= EpisodeRenderer::render(["episode" => $episode]);
         }
+        return <<<FIN
+        <div class="serie">
+            <h2>{$serie->title}</h2>
+            <p>{$serie->description}</p>
+            <p>{$serie->annee}</p>
+            <img src="{$serie->image}" alt="{$serie->title}"/>
+            <div class="episodes">
+                $episodes
+            </div>
+        FIN;
     }
 }
