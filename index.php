9db439e9c3ca5b934c7ba691ab5b9a1c3beadecc
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-04 15:27:56 +0100

    fix de l'index

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-delclare(strict_types=1);
+declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 session_start(); // après l'autoload pour éviter les erreurs de classes non trouvées
 
-use netvod\dispatcher\Dispatcher;
+use netvod\dispatch\Dispatcher;
 
-$dispatcher = new Dispatcher();
+$dispatcher = new Dispatcher($_GET['action'] ?? '');
 $dispatcher->run();
