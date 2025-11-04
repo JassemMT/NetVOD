1c25db2344b4ae0e618f92e1d57b8b63e49b2201
Jassem <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-04 15:55:02 +0100

    Implement Database class for PDO connection

---
+++ src/core/Database.php
@@ -0,0 +1,48 @@
+<?php
+namespace netvod\core;
+
+use PDO;
+
+class Database {
+    private \PDO $pdo;
+    private static ?DeefyRepository $instance = null;
+    private static array $config = [];
+
+    private function __construct( array $conf ){
+        $this->pdo = new \PDO( $conf['dsn'], $conf['user'], $conf['pass'],
+            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+    }
+
+    public static function getInstance(){
+        if (is_null(self::$instance)){
+            self::$instance = new DeefyRepository(self::$config);
+        }
+        return self::$instance;
+    }
+
+    public static function setConfig( $file ){
+        $conf = parse_ini_file( $file );
+
+        if($conf===false){
+            throw new \Exception("Fichier de configuration introuvable");
+        }
+
+        $driver = $conf['driver'] ?? 'mysql';
+        $host   = $conf['host'] ?? 'localhost';
+        $dbname = $conf['database'] ?? '';
+        if (!$dbname) {
+            throw new \RuntimeException("Le fichier de configuration doit contenir 'database'.");
+        }
+
+        $dsn = "$driver:host=$host;dbname=$dbname";
+        if (!empty($conf['charset'])) {
+            $dsn .= ";charset={$conf['charset']}";
+        }
+
+        self::$config = [
+            'dsn'  => $dsn,
+            'user' => $conf['username'] ?? $conf['user'] ?? '',
+            'pass' => $conf['password'] ?? $conf['pass'] ?? ''
+        ];
+    }
+}
