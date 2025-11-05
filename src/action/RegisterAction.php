f560f19fca6a8a17f9066954d671c7c2455dda63
JassemMT <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-05 16:22:41 +0100

    Changement du nom : RegisterAction.php

--- src/action/RegisterAction.php
+++ src/action/RegisterAction.php
@@ -2,12 +2,12 @@
 declare(strict_types=1);
 namespace netvod\action;
 
-use netvod\auth\AuthnProvider;
 use netvod\exception\BadRequestMethodException;
 use netvod\exception\MissingArgumentException;
 use netvod\exception\InvalidArgumentException;
+use netvod\repository\UserRepository;
 
-class SignInAction implements Action {
+class RegisterAction implements Action {
 
     public function execute() : string {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -43,21 +43,58 @@ class SignInAction implements Action {
                     </div>
                     FIN;
         } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['mail'])) {
-                if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) !== null) {
-                    if (isset($_POST['password1']) && isset($_POST['password2'])){
-                        if (!empty($_POST['password1'] && !empty($_POST['password2']))) {
-                            $mail = $_POST['mail'];
-                            $pswd1 = $_POST['password1'];
-                            $pswd2 = $_POST['password2'];
-                            if ($pswd1 === $pswd2) {
-                                AuthnProvider::register($mail, $pswd1);
-                                return '';
-                            } else throw new InvalidArgumentException("password");
-                        } else throw new InvalidArgumentException("password");
-                    } else throw new MissingArgumentException("password");
-                } else throw new InvalidArgumentException("email");
-            } else throw new MissingArgumentException("email");
+            // Vérification des champs obligatoires
+            if (!isset($_POST['mail'])) throw new MissingArgumentException("email");
+            if (!isset($_POST['password1']) || !isset($_POST['password2'])) throw new MissingArgumentException("password");
+
+            $mail = trim((string)($_POST['mail'] ?? ''));
+            $pswd1 = (string)($_POST['password1'] ?? '');
+            $pswd2 = (string)($_POST['password2'] ?? '');
+
+            if (filter_var($mail, FILTER_VALIDATE_EMAIL) === false) {
+                throw new InvalidArgumentException("email");
+            }
+
+            if ($pswd1 === '' || $pswd2 === '') {
+                throw new InvalidArgumentException("password");
+            }
+
+            if ($pswd1 !== $pswd2) {
+                throw new InvalidArgumentException("password");
+            }
+
+            if (mb_strlen($pswd1) < 10) {
+                throw new InvalidArgumentException("password");
+            }
+
+            // Utilise le UserRepository pour la création de l'utilisateur
+            $repo = UserRepository::getInstance();
+
+            // Vérifie qu'il n'existe pas déjà un utilisateur avec cet email
+            $existing = $repo->findUserByEmail($mail);
+            if ($existing !== null) {
+                throw new InvalidArgumentException("email");
+            }
+
+            $hash = password_hash($pswd1, PASSWORD_DEFAULT, ['cost' => 12]);
+            if ($hash === false) {
+                throw new \RuntimeException('Erreur de hachage du mot de passe');
+            }
+
+            try {
+                $user = $repo->createUser($mail, $hash);
+
+                // Initialise la session si nécessaire et stocke l'utilisateur
+                if (session_status() !== PHP_SESSION_ACTIVE) {
+                    session_start();
+                }
+                $_SESSION['user'] = $user;
+
+                return '';
+            } catch (\Exception $e) {
+                throw new \RuntimeException('Erreur création utilisateur');
+            }
+
         } else throw new BadRequestMethodException();
     }
 }
