4e3c992b86b9565e7f57e4a684f193b87cac4564
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-05 14:51:53 +0100

    conflit

--- src/action/DisplayEpisodeAction.php
+++ src/action/DisplayEpisodeAction.php
@@ -6,7 +6,6 @@ use netvod\renderer\EpisodeRenderer;
 use netvod\exception\BadRequestMethodException;
 
 class DisplayEpisodeAction implements Action {
-<<<<<<< HEAD
     public static function execute(): string {
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
             $rep = EpisodeRepository::GetInstance();
@@ -19,15 +18,5 @@ class DisplayEpisodeAction implements Action {
             $notation = new NotationAction();
             $html .= $notation->execute();
         } else throw new BadRequestMethodException();
-=======
-    public function execute(): string {
-        
-        $rep = EpisodeRepository::GetInstance(); 
-
-        $episode = $rep->afficher();
-        var_dump($episode);
-
-        return EpisodeRenderer::render(["episode" => $episode]);
->>>>>>> 069b4692620ef18cbb063bd81a989b9ab4fdefb6
     }
 }
