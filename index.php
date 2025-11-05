89ccfbd9263381c944b0ceb36666baf75c064fbc
JassemMT <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-05 14:49:52 +0100

    import des ressources Instantation de la Database

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 session_start(); // après l'autoload pour éviter les erreurs de classes non trouvées
 
+
 use netvod\dispatch\Dispatcher;
+use netvod\core\Database;
+
+
+Database::setConfig( __DIR__ . '/config/config.db.ini');
 
 $dispatcher = new Dispatcher($_GET['action'] ?? '');
 $dispatcher->run();
