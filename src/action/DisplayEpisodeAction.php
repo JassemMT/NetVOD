a8828960446c77d7226c7958460abba098233c0e
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-05 14:49:07 +0100

    modif notation

--- src/action/DisplayEpisodeAction.php
+++ src/action/DisplayEpisodeAction.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 namespace netvod\action;
 
 use netvod\renderer\EpisodeRenderer;
+use netvod\exception\BadRequestMethodException;
 
 class DisplayEpisodeAction implements Action {
     public static function execute(): string {
-        
-        $rep = EpisodeRepository::GetInstance(); 
-
-        $episode = $rep->afficher();
-        var_dump($episode);
-
-        return EpisodeRenderer::render(["episode" => $episode]);
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+            $rep = EpisodeRepository::GetInstance();
+    
+            $episode = $rep->afficher();
+            var_dump($episode);
+    
+            $html = "";
+            $html .= EpisodeRenderer::render(["episode" => $episode]);
+            $notation = new NotationAction();
+            $html .= $notation->execute();
+        } else throw new BadRequestMethodException();
     }
 }
