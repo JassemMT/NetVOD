c0453b366a02cf612b3400799ff38f38e1a84955
JassemMT <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-07 11:30:27 +0100

    ajout methode de test

--- src/repository/EpisodeRepository.php
+++ src/repository/EpisodeRepository.php
@@ -2,3 +2,5 @@ findById(int $id_episode): ?Episode
 findBySerie(int $id_serie): array
 
 markAsWatching(int $id_user, int $id_serie): bool  // ajoute à "en cours"
+
+create(Episode $episode): Episode;
