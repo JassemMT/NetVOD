39e84be59d0ee3a0e6628a39d9e9e9a47ebdc92b
Thomas Goffin <thomas.goffin.001@student.uni.lu>
2025-11-11 13:00:35 +0100

    Add Encours fait + enleve au dernier ep

--- src/action/DisplayEpisodeAction.php
+++ src/action/DisplayEpisodeAction.php
@@ -8,6 +8,7 @@ use netvod\renderer\EpisodeRenderer;
 use netvod\exception\BadRequestMethodException;
 use netvod\renderer\form\NotationFormRenderer;
 use netvod\repository\EpisodeRepository;
+use netvod\repository\UserRepository;
 use netvod\exception\ActionUnauthorizedException;
 
 class DisplayEpisodeAction implements Action {
@@ -25,6 +26,23 @@ class DisplayEpisodeAction implements Action {
                     $html = "";
                     $renderer = new EpisodeRenderer($episode);
                     $html .= $renderer->render();
+                    //var_dump($episode->id);
+                    // test si l'id de l'épisode correspond à l'id du premier épisode d'une des séries 
+                    if ($episode->id == 1 || $episode->id == 6 || $episode->id == 11 || $episode->id == 14 || $episode->id == 17 ||$episode->id == 20 ){
+                        $id_user = $_SESSION['user'];
+                        $listName = 'en_cours';
+                        $r = UserRepository::addSerieToList($id_user, $id_serie, $listName);
+                        //var_dump($r);
+                    }
+                    // test si l'id de l'épisode correspond à l'id du dernier épisode d'une des séries 
+                    elseif ($episode->id == 5 || $episode->id == 10 || $episode->id == 13 || $episode->id == 16 || $episode->id == 19 ||$episode->id == 21 ) {
+                        $id_user = $_SESSION['user'];
+                        $listName = 'en_cours';
+                        $r = UserRepository::removeSerieFromList($id_user, $id_serie, $listName);
+                        //var_dump($r);   
+                    }
+
+
                     $renderer = new NotationFormRenderer($id_serie);
                     $html .= $renderer->render();
                     return $html;
