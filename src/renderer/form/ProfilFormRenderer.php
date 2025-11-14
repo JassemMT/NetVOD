97cf2051c1729ef9f7bbb4e2b9b7f108bf808746
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-11-14 08:24:08 +0100

    ajout du style pour le formulaire de modification des info de profil

--- src/renderer/form/ProfilFormRenderer.php
+++ src/renderer/form/ProfilFormRenderer.php
@@ -7,15 +7,28 @@ use netvod\renderer\Renderer;
 class ProfilFormRenderer implements Renderer {
     public function render(): string {
         return <<<FIN
-        <form method="post" action="?action=profil-info">
-            <label for="nom">Nom :</label>
-            <input type="text" id="nom" name="nom" />
-            <br/>
-            <label for="prenom">Prénom :</label>
-            <input type="text" id="prenom" name="prenom" />
-            <br/>
-            <button type="submit">Enregistrer</button>
-        </form>
+        <section class="profile-edit container" aria-labelledby="profile-edit-title">
+            <header>
+                <h1 id="profile-edit-title">Modifier mes informations</h1>
+            </header>
+
+            <form method="post" action="?action=profil-info" class="profile-edit-form" novalidate>
+                <div class="form-row">
+                    <label for="nom">Nom</label>
+                    <input id="nom" name="nom" type="text" />
+                </div>
+
+                <div class="form-row">
+                    <label for="prenom">Prénom</label>
+                    <input id="prenom" name="prenom" type="text" />
+                </div>
+
+                <div class="form-actions" style="margin-top:1rem;">
+                    <button type="submit" class="btn btn-primary">Enregistrer</button>
+                    <a class="btn" href="?action=profile">Annuler</a>
+                </div>
+            </form>
+        </section>
         FIN;
     }
 }
